99a3856fd9c75d9f2926a0cfb1dea3da94ae54de
Arturo Torres Sánchez <r2d2.art2005@gmail.com>
2016-05-10 12:25:50 -0500

    El multMatrix de una dimensión regresa también una dimensión

--- src/tc2011/proyectofinal/Helpers.java
+++ src/tc2011/proyectofinal/Helpers.java
@@ -32,12 +32,11 @@ public class Helpers
 		return temp;
 	}
 
-	public static double[][] multMatrix(double a[][], double b[])
+	public static double[] multMatrix(double a[][], double b[])
 	{
-		//a[m][n], b[n][p]
 		if (a.length == 0)
 		{
-			return new double[0][0];
+			return new double[0];
 		}
 		if (a[0].length != b.length)
 		{
@@ -45,19 +44,15 @@ public class Helpers
 		}
 		int n = a[0].length;
 		int m = a.length;
-		int p = 1;
 
-		double ans[][] = new double[m][p];
+		double ans[] = new double[m];
 
 		for (int i = 0; i < m; i++)
 		{
-			for (int j = 0; j < p; j++)
+			ans[i] = 0;
+			for (int k = 0; k < n; k++)
 			{
-				ans[i][j] = 0;
-				for (int k = 0; k < n; k++)
-				{
-					ans[i][j] += a[i][k] * b[k];
-				}
+				ans[i] += a[i][k] * b[k];
 			}
 		}
 		return ans;
