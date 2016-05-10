abcbfd7975bb22ff2dce5c9b3f5d9f674b64d14e
Arturo Torres Sánchez <r2d2.art2005@gmail.com>
2016-05-10 12:21:19 -0500

    (por esto debimos haber usado Matlab)

--- src/tc2011/proyectofinal/Helpers.java
+++ src/tc2011/proyectofinal/Helpers.java
@@ -19,6 +19,50 @@ public class Helpers
 		return IntStream.range(startInclusive, endExclusive).mapToDouble(function).sum();
 	}
 
+	public static double[][] transposeMatrix(double[][] m)
+	{
+		double[][] temp = new double[m[0].length][m.length];
+		for (int i = 0; i < m.length; i++)
+		{
+			for (int j = 0; j < m[0].length; j++)
+			{
+				temp[j][i] = m[i][j];
+			}
+		}
+		return temp;
+	}
+
+	public static double[][] multMatrix(double a[][], double b[])
+	{
+		//a[m][n], b[n][p]
+		if (a.length == 0)
+		{
+			return new double[0][0];
+		}
+		if (a[0].length != b.length)
+		{
+			throw new RuntimeException();
+		}
+		int n = a[0].length;
+		int m = a.length;
+		int p = 1;
+
+		double ans[][] = new double[m][p];
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < p; j++)
+			{
+				ans[i][j] = 0;
+				for (int k = 0; k < n; k++)
+				{
+					ans[i][j] += a[i][k] * b[k];
+				}
+			}
+		}
+		return ans;
+	}
+
 	public static double[][] multMatrix(double a[][], double b[][])
 	{
 		//a[m][n], b[n][p]
